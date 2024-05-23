a42e5bdad9db7df18f7048c8e6a994bc660b426f
B1228015 <161295861+B1228015@users.noreply.github.com>
2024-05-23 14:01:10 +0800

    Create main.cpp

---
+++ EX5/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class myString
+{
+    private:
+        string m_str;
+    public:
+    myString(string s)
+    {
+        m_str = s;
+    }
+};
+
+class Readclass : myString
+{
+    public:
+        in.open("main.cpp");
+        if (in.fail())
+        {
+            return 0;
+        }
+    while()
+
+    private:
+        int num;
+        char name[];
+
+};
+
+int main()
+{
+    Readclass rfile;
+    rfile.showClass();
+    return 0;
+}
